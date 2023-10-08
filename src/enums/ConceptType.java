5a632508884d4798a1a237032c03b7ee4fbcc9a0
YohanDevPs <yohanps.dev@gmail.com>
2023-10-08 14:08:30 -0300

    refactor: translate description enums from to spanish

--- src/enums/ConceptType.java
+++ src/enums/ConceptType.java
@@ -1,21 +1,21 @@
 package enums;
 
 public enum ConceptType {
-    CURRENT_MONEY(AssetType.ACTIVE_CURRENT, "Current Money"),
-    BILL_TO_RECEIVE(AssetType.ACTIVE_CURRENT, "Bill to Receive"),
-    EFECTIVO(AssetType.ACTIVE_CURRENT, "Efectivo"),
+    CURRENT_MONEY(AssetType.ACTIVE_CURRENT, "Dinero actual"),
+    BILL_TO_RECEIVE(AssetType.ACTIVE_CURRENT, "Cuentas para recibir"),
+    EFECTIVO(AssetType.ACTIVE_CURRENT, "Efectivos"),
 
-    PROPERTY(AssetType.FIXED_ACTIVE, "Property"),
-    TEAM(AssetType.FIXED_ACTIVE, "Team"),
-    DEFERRED_ASSETS(AssetType.FIXED_ACTIVE, "Deferred Assets"),
+    PROPERTY(AssetType.FIXED_ACTIVE, "Propriedades"),
+    TEAM(AssetType.FIXED_ACTIVE, "Equipos"),
+    DEFERRED_ASSETS(AssetType.FIXED_ACTIVE, "Activos diferidos"),
 
-    TAX(AssetType.PASSIVE_CURRENT, "Tax"),
-    BILL_TO_PAY(AssetType.PASSIVE_CURRENT, "Bill to Pay"),
-    RESERVE_PROVISION(AssetType.PASSIVE_CURRENT, "Reserve Provision"),
+    TAX(AssetType.PASSIVE_CURRENT, "Impuesto"),
+    BILL_TO_PAY(AssetType.PASSIVE_CURRENT, "Factura para pagar"),
+    RESERVE_PROVISION(AssetType.PASSIVE_CURRENT, "Provisión de reserva"),
 
-    BONUSES(AssetType.PASSIVE_LONG_TERM, "Bonuses"),
-    FINANCE_LEASES(AssetType.PASSIVE_LONG_TERM, "Finance Leases"),
-    LONG_TERM_LOAN(AssetType.PASSIVE_LONG_TERM, "Long Term Loan");
+    BONUSES(AssetType.PASSIVE_LONG_TERM, "Bonificaciones"),
+    FINANCE_LEASES(AssetType.PASSIVE_LONG_TERM, "Arrendamientos financieros"),
+    LONG_TERM_LOAN(AssetType.PASSIVE_LONG_TERM, "Préstamo a largo plazo");
 
     private final AssetType parentType;
     private final String description;
